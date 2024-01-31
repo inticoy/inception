8bff7410a4b5b9360ebc0e47e403f6b296489b58
inticoy <inticoy0406@gmail.com>
2024-01-31 22:59:30 +0900

    feat: mariadb with docker compose

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,7 +1,33 @@
+# allow other container can access mariadb
+mv /etc/mysql/mariadb.conf.d/50-server.cnf /etc/mysql/mariadb.conf.d/50-server-default.cnf
+cat /etc/mysql/mariadb.conf.d/50-server-default.cnf | sed 's/127.0.0.1/0.0.0.0/' > /etc/mysql/mariadb.conf.d/50-server.cnf
+
+# start a mariadb process
 service mariadb start
 
-echo "CREATE DATABASE wordpress;"
-echo "CREATE DATABASE wordpress;"| mariadb
-echo "CREATE USER 'wordpressuser'@'localhost' IDENTIFIED BY 'password';" | mariadb
-echo "GRANT ALL ON wordpress.* TO 'wordpressuser'@'localhost';" | mariadb
-echo "FLUSH PRIVILEGES;" | mariadb
+# 있으나 없으나 왜 에러가 나지
+#gyoon-mariadb  | /usr/bin/mysqladmin: connect to server at 'localhost' failed
+#gyoon-mariadb  | error: 'Can't connect to local MySQL server through socket '/run/mysqld/mysqld.sock' (2)'
+#gyoon-mariadb  | Check that mysqld is running and that the socket: '/run/mysqld/mysqld.sock' exists!
+# while ! mysqladmin ping;
+# do
+# 	sleep 2
+# done
+
+# make a database
+echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" | mariadb -u root
+
+# make a mariadb user
+echo "CREATE USER IF NOT EXISTS '$DB_USER'@'$DB_HOST' IDENTIFIED BY '$DB_PWD';" | mariadb -u root
+
+# grant all privileges to user on database : user can perform all operations (SELECT, INSERT, ...) on tables within $DB_NAME
+echo "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'$DB_HOST';" | mariadb -u root
+
+# reload/apply new privilege tables immediately
+echo "FLUSH PRIVILEGES;" | mariadb -u root
+
+# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root12345';" | mariadb -u root 
+
+service mariadb stop
+
+mysqld
